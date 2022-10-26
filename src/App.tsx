1102e78de46db34977737cdeb0057a158a36e717
Léa Hamadouche <leahamadouche@hotmail.com>
2022-10-26 23:19:57 +0200

    feat(theme): :art: Added switch theme button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
     CssBaseline,
     GlobalStyles,
@@ -6,16 +6,25 @@ import {
     useMediaQuery,
 } from '@mui/material'
 import { ThemeConfig } from './assets/theme/ThemeConfig'
+import SwitchThemeButton from './assets/theme/SwitchThemeButton'
 
 import './App.css'
 
 function App() {
     // Récupère l'apparence en fonction des choix de l'utilisateur sur sa machine
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-    const themeConfig = ThemeConfig(prefersDarkMode)
-
+    const [darkMode, setDarkMode] = React.useState(prefersDarkMode)
+    const themeConfig = ThemeConfig(darkMode)
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDarkMode(event.target.checked)
+    }
     // Le thème se rafraîchit automatiquement à chaque changement de la valeur de themeConfig
     const theme = React.useMemo(() => themeConfig, [themeConfig])
+
+    // Met à jour le thème en fonction des choix de l'utilisateur sur sa machine
+    useEffect(() => {
+        setDarkMode(prefersDarkMode)
+    }, [prefersDarkMode])
 
     return (
         <ThemeProvider theme={theme}>
@@ -25,6 +34,7 @@ function App() {
                     body: { backgroundColor: theme.palette.background.default },
                 }}
             />
+            <SwitchThemeButton checked={darkMode} onChange={handleChange} />
         </ThemeProvider>
     )
 }
