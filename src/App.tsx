ff7acc6dd813238a3499fe00099593799cf6b82b
Léa Hamadouche <leahamadouche@hotmail.com>
2022-10-29 16:03:47 +0200

    feat(pages): :new: Added all pages and their route

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,19 @@ import {
 } from '@mui/material'
 import { ThemeConfig } from './assets/theme/ThemeConfig'
 import Layout from './layouts/Layout'
+import Homepage from './pages/Homepage'
+import Offers from './pages/Offers'
+import MyProfile from './pages/MyProfile'
+import PublicProfile from './pages/PublicProfile'
+import MyOffers from './pages/MyOffers'
+import MyOffer from './pages/MyOffer'
+import Offer from './pages/Offer'
+import Logout from './pages/Logout'
+import Login from './pages/Login'
+import Signin from './pages/Signin'
 
 function App() {
-    const [loggedIn] = React.useState(true) //TODO : Finish this when connection functionality is done
+    const [loggedIn, setLoggedIn] = React.useState(true) //TODO: Léa - Finish this when connection functionality is done
 
     // Récupère l'apparence en fonction des choix de l'utilisateur sur sa machine
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
@@ -18,6 +28,7 @@ function App() {
     const themeConfig = ThemeConfig(darkMode)
     const handleChangeMode = (event: React.ChangeEvent<HTMLInputElement>) => {
         setDarkMode(event.target.checked)
+        setLoggedIn(!loggedIn) //TODO: Léa - Remove this when connection functionality is done
     }
     // Le thème se rafraîchit automatiquement à chaque changement de la valeur de themeConfig
     const theme = React.useMemo(() => themeConfig, [themeConfig])
@@ -38,25 +49,42 @@ function App() {
                     },
                 }}
             />
+
             <Router>
-                <Routes>
-                    <Route path="/" />
-                    <Route path="/" />
-                    <Route path="/" />
-                    <Route path="/" />
-                    <Route path="/" />
-                    <Route path="/" />
-                    <Route path="/" />
-                </Routes>
+                <Layout
+                    {...{
+                        theme,
+                        loggedIn,
+                        darkMode,
+                        handleChangeMode,
+                    }}
+                >
+                    <Routes>
+                        {/* TODO: Léa - Change links and components props with the right params */}
+                        <Route path="/">
+                            <Route index element={<Homepage />} />
+                            <Route path="/annonces" element={<Offers />} />
+                            <Route path="/mon-profil" element={<MyProfile />} />
+                            <Route
+                                path="/profil/:id"
+                                element={<PublicProfile />}
+                            />
+                            <Route
+                                path="/mes-annonces"
+                                element={<MyOffers />}
+                            />
+                            <Route
+                                path="/mon-annonce/:id"
+                                element={<MyOffer />}
+                            />
+                            <Route path="/annonce/:id" element={<Offer />} />
+                            <Route path="/deconnexion" element={<Logout />} />
+                            <Route path="/connexion" element={<Login />} />
+                            <Route path="/inscription" element={<Signin />} />
+                        </Route>
+                    </Routes>
+                </Layout>
             </Router>
-            <Layout
-                {...{
-                    theme,
-                    loggedIn,
-                    darkMode,
-                    handleChangeMode,
-                }}
-            />
         </ThemeProvider>
     )
 }
