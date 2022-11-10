4438e604348aa7c5f73e93f3f473f2063254c7c7
Léa Hamadouche <leahamadouche@hotmail.com>
2022-11-10 14:43:58 +0100

    feat(categories filter): :sparkles: Fixed categories filter

--- src/components/offers/OffersFilter.tsx
+++ src/components/offers/OffersFilter.tsx
@@ -5,22 +5,76 @@ import {
     OutlinedInput,
     InputAdornment,
     FormControl,
+    FormControlLabel,
+    FormLabel,
+    FormGroup,
+    Checkbox,
+    Grid,
+    Button,
 } from '@mui/material'
 import { SectionTitle } from '../common/custom/CustomTitles'
 import { Search } from '@mui/icons-material'
+
+import { RequestType } from '../../shared/types/RequestType'
+import request from '../../api/Request'
+import { Category } from '../../shared/types/Category'
 
 type OffersFilterProps = {
     theme: Theme
     searchValue: string
     // eslint-disable-next-line no-unused-vars
     handleSearchValueChange: (event: React.BaseSyntheticEvent) => void
+    // eslint-disable-next-line no-unused-vars
+    handleCategoryChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+type CheckboxType = {
+    category: string
+    label: string
 }
 
 export default function OffersFilter({
     theme,
     searchValue,
     handleSearchValueChange,
+    handleCategoryChange,
 }: OffersFilterProps) {
+    const [categories, setCategories] = React.useState<Category[]>([])
+    const [checkboxCategories, setCheckboxCategories] = React.useState<
+        CheckboxType[]
+    >([])
+    const [toggleCategories, setToggleCategories] = React.useState(false)
+    const [getCheckboxes, setGetCheckboxes] = React.useState(false)
+
+    const requestParams: RequestType = {
+        endpoint: '/offers-categories',
+        method: 'GET',
+    }
+
+    async function getCategories() {
+        try {
+            await request(requestParams)
+                .then((response) => response.json())
+                .then((data) => setCategories(data))
+        } catch (error: any) {
+            // eslint-disable-next-line no-console
+            console.log(error)
+        }
+    }
+
+    React.useEffect(() => {
+        getCategories()
+        if (getCheckboxes)
+            setCheckboxCategories(
+                categories.map(({ machine_name, label }) => ({
+                    category: machine_name,
+                    label,
+                    checked: false,
+                }))
+            )
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [getCheckboxes])
+
     return (
         <>
             <SectionTitle title="Rechercher une annonce" />
@@ -31,22 +85,72 @@ export default function OffersFilter({
                     padding: 3,
                 }}
             >
-                <FormControl sx={{ m: 1, width: '40%' }} variant="outlined">
-                    <OutlinedInput
-                        value={searchValue}
-                        onChange={handleSearchValueChange}
-                        placeholder="Rechercher"
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <Search />
-                            </InputAdornment>
-                        }
-                        aria-describedby="outlined-weight-helper-text"
-                        inputProps={{
-                            'aria-label': 'weight',
-                        }}
-                    />
-                </FormControl>
+                <Grid container justifyContent="center" m={1}>
+                    <Grid item md={6}>
+                        <OutlinedInput
+                            fullWidth
+                            value={searchValue}
+                            onChange={handleSearchValueChange}
+                            placeholder="Rechercher"
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <Search />
+                                </InputAdornment>
+                            }
+                            aria-describedby="outlined-weight-helper-text"
+                            inputProps={{
+                                'aria-label': 'weight',
+                            }}
+                            sx={{ justifyContent: 'center' }}
+                        />
+                        <Button
+                            onClick={() => {
+                                setToggleCategories(!toggleCategories)
+                                setGetCheckboxes(!getCheckboxes)
+                            }}
+                            sx={{
+                                my: 2,
+                                color: theme.palette.primary.main,
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            + de filtres
+                        </Button>
+                    </Grid>
+
+                    {toggleCategories && (
+                        <Grid container justifyContent="center">
+                            <FormControl
+                                component="fieldset"
+                                variant="standard"
+                            >
+                                <Grid item xs={12}>
+                                    <FormLabel component="legend">
+                                        Choisir une ou plusieurs catégorie(s)
+                                    </FormLabel>
+                                    <FormGroup row>
+                                        {checkboxCategories.map(
+                                            ({ label, category }, index) => (
+                                                <FormControlLabel
+                                                    key={index}
+                                                    control={
+                                                        <Checkbox
+                                                            onChange={
+                                                                handleCategoryChange
+                                                            }
+                                                            value={category}
+                                                        />
+                                                    }
+                                                    label={label}
+                                                />
+                                            )
+                                        )}
+                                    </FormGroup>
+                                </Grid>
+                            </FormControl>
+                        </Grid>
+                    )}
+                </Grid>
             </Card>
         </>
     )
