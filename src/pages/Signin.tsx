37509352fd0149b168ebc849d9fa5d9dfd343b1c
Adrian SOREAU <adrian.soreau@42c.fr>
2022-11-10 16:51:06 +0100

    fix(Singin): fix post request with files

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Grid, TextField } from '@mui/material'
+import { Button, Grid, TextField, Typography } from '@mui/material'
 import { RequestType } from '../shared/types/RequestType'
 import request from '../api/Request'
 
@@ -9,44 +9,88 @@ export default function Signin() {
         mb: '0.5rem',
     }
 
-    const [email, setEmail] = useState<string | null>('')
-    const [password, setPassword] = useState<string | null>('')
-    const [firstname, setFirstname] = useState<string | null>('')
-    const [lastname, setLastname] = useState<string | null>('')
-    const [phone, setPhone] = useState<string | null>('')
-    const [address, setAddress] = useState<string | null>('')
-    const [zipCode, setZipCode] = useState<string | null>('')
-    const [city, setCity] = useState<string | null>('')
-    const [country, setCountry] = useState<string | null>('')
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [firstname, setFirstname] = useState<string>('')
+    const [lastname, setLastname] = useState<string>('')
+    const [phone, setPhone] = useState<string>('')
+    const [address, setAddress] = useState<string>('')
+    const [zipCode, setZipCode] = useState<string>('')
+    const [city, setCity] = useState<string>('')
+    const [country, setCountry] = useState<string>('')
     const [identificalFile, setIdentificalFile] = useState<File | null>(null)
     const [profilePicture, setProfilePicture] = useState<File | null>(null)
 
-    async function register() {
+    const handleIdentificalChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        if (event.target.files && event.target.files[0]) {
+            setIdentificalFile(event.target.files[0])
+        }
+    }
+    const handleRemoveIdentifical = () => {
+        setIdentificalFile(null)
+    }
+
+    const handleProfilChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files[0]) {
+            setProfilePicture(event.target.files[0])
+        }
+    }
+    const handleRemoveProfil = () => {
+        setProfilePicture(null)
+    }
+
+    // async function register() {
+    //     const requestParams: RequestType = {
+    //         endpoint: '/registration',
+    //         method: 'POST',
+    //         body: {
+    //             email,
+    //             password,
+    //             firstname,
+    //             lastname,
+    //             phone,
+    //             address,
+    //             zip_code: zipCode,
+    //             city,
+    //             country,
+    //             identifical_file: identificalFile,
+    //             profile_picture: profilePicture,
+    //         },
+    //     }
+    //     try {
+    //         await request(requestParams).then((response) => response.json())
+    //     } catch (error: any) {
+    //         // eslint-disable-next-line no-console
+    //         console.log(error)
+    //     }
+    // }
+    const handleSigninSubmit = async () => {
+        const body = new FormData()
+
+        body.append('email', email)
+        body.append('password', password)
+        body.append('firstname', firstname)
+        body.append('lastname', lastname)
+        body.append('phone', phone)
+        body.append('address', address)
+        body.append('zip_code', zipCode)
+        body.append('city', city)
+        body.append('country', country)
+        if (identificalFile) {
+            body.append('identifical_file', identificalFile)
+        }
+        if (profilePicture) {
+            body.append('profile_picture', profilePicture)
+        }
         const requestParams: RequestType = {
             endpoint: '/registration',
             method: 'POST',
-            body: {
-                email,
-                password,
-                firstname,
-                lastname,
-                phone,
-                address,
-                zip_code: zipCode,
-                city,
-                country,
-                identifical_file: identificalFile,
-                profile_picture: profilePicture,
-            },
+            body,
         }
-        try {
-            await request(requestParams).then((response) => response.json())
-        } catch (error: any) {
-            // eslint-disable-next-line no-console
-            console.log(error)
-        }
+        await request(requestParams)
     }
-
     return (
         <Grid
             container
@@ -129,33 +173,127 @@ export default function Signin() {
                 value={country}
                 onChange={(e) => setCountry(e.target.value)}
             />
-
-            <input
-                type="file"
-                onChange={(e) =>
-                    e.target.files && setIdentificalFile(e.target.files[0])
-                }
-            />
-
-            <input
-                type="file"
-                onChange={(e) => {
-                    // eslint-disable-next-line no-console
-                    console.log(e.target.files)
-                    e.target.files && setProfilePicture(e.target.files[0])
-                    // eslint-disable-next-line no-console
-                    console.log(profilePicture?.name)
-                }}
-            />
-
+            <Grid xs={12}>
+                <Grid
+                    item
+                    xs={12}
+                    container
+                    justifyContent="center"
+                    alignItems="center"
+                    m={1}
+                    p={2}
+                    sx={{ border: '1px solid white', flexDirection: 'column' }}
+                >
+                    <Typography variant="h6">Identifical File :</Typography>
+                    {!identificalFile && (
+                        <Grid
+                            item
+                            md={6}
+                            container
+                            justifyContent="center"
+                            alignItems="center"
+                        >
+                            <Button variant="contained" component="label">
+                                Télécharger une photo
+                                <input
+                                    hidden
+                                    accept="image/*"
+                                    type="file"
+                                    onChange={handleIdentificalChange}
+                                />
+                            </Button>
+                        </Grid>
+                    )}
+                    {identificalFile && (
+                        <Grid
+                            item
+                            md={6}
+                            container
+                            justifyContent="center"
+                            alignItems="center"
+                        >
+                            <img
+                                src={URL.createObjectURL(identificalFile)}
+                                width="80%"
+                                alt="Aperçu du fichier téléchargé"
+                            />
+                            <Button
+                                sx={{
+                                    m: 2,
+                                }}
+                                size="small"
+                                variant="contained"
+                                onClick={handleRemoveIdentifical}
+                            >
+                                Supprimer l'image
+                            </Button>
+                        </Grid>
+                    )}
+                </Grid>
+                <Grid
+                    item
+                    xs={12}
+                    container
+                    justifyContent="center"
+                    alignItems="center"
+                    m={1}
+                    p={2}
+                    sx={{ border: '1px solid white', flexDirection: 'column' }}
+                >
+                    <Typography variant="h6">Profile Picture :</Typography>
+                    {!profilePicture && (
+                        <Grid
+                            item
+                            md={6}
+                            container
+                            justifyContent="center"
+                            alignItems="center"
+                        >
+                            <Button variant="contained" component="label">
+                                Télécharger une photo
+                                <input
+                                    hidden
+                                    accept="image/*"
+                                    type="file"
+                                    onChange={handleProfilChange}
+                                />
+                            </Button>
+                        </Grid>
+                    )}
+                    {profilePicture && (
+                        <Grid
+                            item
+                            md={6}
+                            container
+                            justifyContent="center"
+                            alignItems="center"
+                        >
+                            <img
+                                src={URL.createObjectURL(profilePicture)}
+                                width="80%"
+                                alt="Aperçu du fichier téléchargé"
+                            />
+                            <Button
+                                sx={{
+                                    m: 2,
+                                }}
+                                size="small"
+                                variant="contained"
+                                onClick={handleRemoveProfil}
+                            >
+                                Supprimer l'image
+                            </Button>
+                        </Grid>
+                    )}
+                </Grid>
+            </Grid>
             <Button
                 //Change style
-                onClick={() => register()}
+                variant="contained"
+                onClick={() => handleSigninSubmit()}
                 sx={{
-                    backgroundColor: 'white',
-                    width: '80%',
                     margin: '3rem auto',
-                    color: '#051F38',
+                    width: '50%',
                 }}
             >
                 S'inscrire
